3fd9bca4d9d6981bf330b6f3548010951d2702ca
chengchuanqiang <1625320316@qq.com>
2018-01-07 23:22:16 +0800

    pom中增加jpa操作

---
+++ src/main/java/com/ccq/Girl.java
@@ -0,0 +1,46 @@
+package com.ccq;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author ccq
+ * @Description
+ * @date 2018/1/7 22:23
+ */
+@Entity
+public class Girl {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String cupSize;
+    private Integer age;
+
+    public Girl() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/com/ccq/HelloController.java
+++ src/main/java/com/ccq/HelloController.java
@@ -1,6 +1,5 @@
 package com.ccq;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${cupSize}")
-    private String cupSize;
-
-    @Value("${age}")
-    private Integer age;
-
-    @Value("${content}")
-    private String content;
-
     @RequestMapping(value = "hello",method = RequestMethod.GET)
     public String say(){
-        return content;
+        return "AAA";
     }
 
 }
